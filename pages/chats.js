8bafa53a36b60e1bad415529d729171f4405af4b
gran1um <spiderfan02@gmail.com>
2021-12-14 11:09:45 +0300

    1.0.1

--- pages/chats.js
+++ pages/chats.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useContext} from "react";
+import { Context } from "../context";
 
 export default function Chats() {
   return <div className="background">chats</div>;
